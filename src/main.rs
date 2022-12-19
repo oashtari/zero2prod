39c6e987113cb31026659f418ad882e99f149618
Omid Ashtari <omid@Omids-Mac-mini.local>
2022-12-18 20:29:51 -0800

    a bunch of stuff, forgot to push, starting migrations next

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
