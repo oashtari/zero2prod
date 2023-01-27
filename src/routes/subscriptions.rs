ac7490df395318ad4c66aa2a315ddb205ab6751e
Omid Ashtari <omid@Omids-Mac-mini.local>
2023-01-27 06:44:39 -0800

    chnging sql query back

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -134,8 +134,8 @@ pub async fn subscribe(
 
     // match -- ELIMINATING THIS AFTER ALL 
         sqlx::query!(
-        r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)"#,
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'confirmed')"#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
