c7db4d20331eae9716ceab4e93c20f321784561a
Omid Ashtari <omid@Omids-Mac-mini.local>
2023-01-30 05:32:20 -0800

    minor fix, deleted a bunch of postgres tables, running to try to deploy again

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -4,7 +4,7 @@ use sqlx::{Connection, Executor, PgConnection, PgPool};
 // use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
-// use zero2prod::email_client::EmailClient;
+// use zero2prod::email_cliendot::EmailClient;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::startup::{get_connection_pool, Application};
 
