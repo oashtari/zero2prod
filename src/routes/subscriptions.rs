22892b64a15a30c7f0237dffef55c901fa68c562
Omid Ashtari <omid@Omids-Mac-mini.local>
2023-01-31 20:42:03 -0800

    refactored send  email confirmation code

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -85,27 +85,56 @@ pub async fn subscribe(
         return HttpResponse::InternalServerError().finish();
     }
 
-    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+    if send_confirmation_email(&email_client, new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+
+    #[tracing::instrument(
+        name = "Send a confirmation email to a new subscriber", 
+        skip(email_client, new_subscriber)
+    )]
+
+    pub async fn send_confirmation_email(
+        email_client: &EmailClient,
+        new_subscriber: NewSubscriber,
+    ) -> Result<(), reqwest::Error> {
+        let confirmation_link = "https://my-api.com/subscriptions/confirm";
+        let plain_body = format!(
+            "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+            confirmation_link
+        );
+        let html_body = format!(
+            "Welcome to our newsletter!<br />\
+            Click <a href=\"{}\">here</a> to confirm your subscription.",
+            confirmation_link
+        );
+        email_client
+            .send_email(new_subscriber.email, "Welcome", &html_body, &plain_body)
+            .await
+    }
+
+
+    // ADDED TO ABOVE
     // Send a (useless) email to the new subscriber.
     // We are ignoring email delivery errors for now.
-    if email_client
-        .send_email(
-            new_subscriber.email, 
-            "Welcome", 
-            &format!(
-                "Welcome to our newsletter!<br />\
-                Click <a href=\"{}\">here</a> to confirm your subscription.",
-                confirmation_link
-            ), 
-            &format!(
-                "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
-                confirmation_link
-            ),
-        )
-        .await
-        .is_err() {
-            return HttpResponse::InternalServerError().finish();
-        }
+        // email_client
+        //     .send_email(
+        //         new_subscriber.email, 
+        //         "Welcome", 
+        //         &format!(
+        //             "Welcome to our newsletter!<br />\
+        //             Click <a href=\"{}\">here</a> to confirm your subscription.",
+        //             confirmation_link
+        //         ), 
+        //         &format!(
+        //             "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        //             confirmation_link
+        //         ),
+        //     )
+        //     .await
+        //     .is_err() {
+        //         return HttpResponse::InternalServerError().finish();
+        //     }
 
         HttpResponse::Ok().finish()
 }
